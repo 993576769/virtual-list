056372d067684dcef416e396db01bfe6ad13f168
993576769 <993576769@users.noreply.github.com>
2024-10-08 09:07:38 +0000

    deploy: aca442ff2e0bd79f55ecb68f33d10c61d9aeb0f2

---
+++ item.vue.d.ts
@@ -0,0 +1,28 @@
+import { PropType } from 'vue';
+declare const _default: <T>(__VLS_props: NonNullable<Awaited<typeof __VLS_setup>>["props"], __VLS_ctx?: __VLS_PrettifyLocal<Pick<NonNullable<Awaited<typeof __VLS_setup>>, "attrs" | "emit" | "slots">>, __VLS_expose?: NonNullable<Awaited<typeof __VLS_setup>>["expose"], __VLS_setup?: Promise<{
+    props: __VLS_PrettifyLocal<Pick<Partial<{}> & Omit<{
+        readonly resize: (height: number) => void;
+        readonly item: [{
+            type: PropType<T>;
+            required: true;
+        }] extends [import('vue').Prop<infer V, infer D>] ? unknown extends V ? keyof V extends never ? import('@vue/shared').IfAny<V, V, D> : V : V : {
+            type: PropType<T>;
+            required: true;
+        };
+        readonly index: number;
+    } & import('vue').VNodeProps & import('vue').AllowedComponentProps & import('vue').ComponentCustomProps, never>, "resize" | "item" | "index"> & {}> & import('vue').PublicProps;
+    expose(exposed: import('vue').ShallowUnwrapRef<{}>): void;
+    attrs: any;
+    slots: {
+        default?(_: {}): any;
+    };
+    emit: {};
+}>) => import('vue').VNode<import('vue').RendererNode, import('vue').RendererElement, {
+    [key: string]: any;
+}> & {
+    __ctx?: Awaited<typeof __VLS_setup>;
+};
+export default _default;
+type __VLS_PrettifyLocal<T> = {
+    [K in keyof T]: T[K];
+} & {};
---
+++ list.vue.d.ts
@@ -0,0 +1,91 @@
+import { PropType, Ref } from 'vue';
+export interface VirtualScrollEmits {
+    (e: 'topArrived'): void;
+    (e: 'bottomArrived'): void;
+    (e: 'scroll'): void;
+}
+export interface RenderedItem<T> {
+    index: number;
+    data: T;
+    offset: number;
+    height: number;
+}
+export interface VirtualScrollInstance<T> {
+    visibleItems: Ref<RenderedItem<T>[]>;
+    renderItems: Ref<RenderedItem<T>[]>;
+    scrollToItem: (options: {
+        key: T[keyof T];
+        alignment: 'start' | 'center' | 'end';
+        smooth?: boolean;
+    }) => Promise<void>;
+    scrollToTop: (smooth?: boolean) => void;
+    scrollToBottom: (smooth?: boolean) => Promise<void>;
+    getScroll: () => {
+        scrollHeight: number;
+        scrollTop: number;
+        clientHeight: number;
+    };
+}
+declare const _default: <T>(__VLS_props: NonNullable<Awaited<typeof __VLS_setup>>["props"], __VLS_ctx?: __VLS_PrettifyLocal<Pick<NonNullable<Awaited<typeof __VLS_setup>>, "attrs" | "emit" | "slots">>, __VLS_expose?: NonNullable<Awaited<typeof __VLS_setup>>["expose"], __VLS_setup?: Promise<{
+    props: __VLS_PrettifyLocal<Pick<Partial<{
+        estimatedItemHeight: number;
+        buffer: number;
+        keyField: [{
+            type: PropType<keyof T>;
+            default: string;
+        }] extends [import('vue').Prop<infer V, infer D>] ? unknown extends V ? keyof V extends never ? import('@vue/shared').IfAny<V, V, D> : V : V : {
+            type: PropType<keyof T>;
+            default: string;
+        };
+        topThreshold: number;
+        bottomThreshold: number;
+        initialPosition: [{
+            type: PropType<"top" | "bottom" | T[keyof T]>;
+            default: string;
+        }] extends [import('vue').Prop<infer V, infer D>] ? unknown extends V ? keyof V extends never ? import('@vue/shared').IfAny<V, V, D> : V : V : {
+            type: PropType<"top" | "bottom" | T[keyof T]>;
+            default: string;
+        };
+    }> & Omit<{
+        readonly items: T[];
+        readonly estimatedItemHeight: number;
+        readonly buffer: number;
+        readonly keyField: [{
+            type: PropType<keyof T>;
+            default: string;
+        }] extends [import('vue').Prop<infer V, infer D>] ? unknown extends V ? keyof V extends never ? import('@vue/shared').IfAny<V, V, D> : V : V : {
+            type: PropType<keyof T>;
+            default: string;
+        };
+        readonly topThreshold: number;
+        readonly bottomThreshold: number;
+        readonly initialPosition: [{
+            type: PropType<"top" | "bottom" | T[keyof T]>;
+            default: string;
+        }] extends [import('vue').Prop<infer V, infer D>] ? unknown extends V ? keyof V extends never ? import('@vue/shared').IfAny<V, V, D> : V : V : {
+            type: PropType<"top" | "bottom" | T[keyof T]>;
+            default: string;
+        };
+        readonly onScroll?: (() => any) | undefined;
+        readonly onTopArrived?: (() => any) | undefined;
+        readonly onBottomArrived?: (() => any) | undefined;
+    } & import('vue').VNodeProps & import('vue').AllowedComponentProps & import('vue').ComponentCustomProps, "estimatedItemHeight" | "buffer" | "keyField" | "topThreshold" | "bottomThreshold" | "initialPosition">, "onScroll" | "items" | "onTopArrived" | "onBottomArrived" | ("estimatedItemHeight" | "buffer" | "keyField" | "topThreshold" | "bottomThreshold" | "initialPosition")> & {}> & import('vue').PublicProps;
+    expose(exposed: import('vue').ShallowUnwrapRef<VirtualScrollInstance<T>>): void;
+    attrs: any;
+    slots: {
+        default?(_: {
+            item: T;
+            index: number;
+            resize: (height: number) => void;
+        }): any;
+    };
+    emit: VirtualScrollEmits;
+}>) => import('vue').VNode<import('vue').RendererNode, import('vue').RendererElement, {
+    [key: string]: any;
+}> & {
+    __ctx?: Awaited<typeof __VLS_setup>;
+};
+export default _default;
+type __VLS_PrettifyLocal<T> = {
+    [K in keyof T]: T[K];
+} & {};
